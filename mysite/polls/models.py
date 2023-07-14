ad12970363b7766c353364c3d203a56aae30c701
kasimpamuk <kasim.pamuk@metu.edu.tr>
2023-07-14 13:52:39 +0300

    start admin panel with 2 models:question,choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,19 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    extra_field = models.CharField(max_length=200, default="extra field", blank=True, null=True)
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
